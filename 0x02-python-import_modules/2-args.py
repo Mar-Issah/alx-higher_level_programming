d8058e3f261c3cb0bed9909c1718d465e4878d90
Marsiya Issah <masy370@gmail.com>
2023-05-11 19:33:11 -0400

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
 
 if len(sys.argv) == 1:
     print("{:d} arguments.".format(len(sys.argv) - 1))
-if len(sys.argv) > 1:
+if len(sys.argv) == 2:
+    print("{:d} argument:".format(len(sys.argv) - 1))
+if len(sys.argv) > 2:
     print("{:d} arguments:".format(len(sys.argv) - 1))
 if len(sys.argv):
     for idx in range(1, len(sys.argv)):
