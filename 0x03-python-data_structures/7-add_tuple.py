d617ff593b19d7891ee43b2f5578597e39d9dc5b
Mar-Issah <masy370@gmail.com>
2023-05-14 13:53:28 -0400

    create new file 8-multiple_returns.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # if the elements is not present return 0
     a_1 = tuple_a[0] if len(tuple_a) > 0 else 0
     a_2 = tuple_a[1] if len(tuple_a) > 1 else 0
     b_1 = tuple_b[0] if len(tuple_b) > 0 else 0
     b_2 = tuple_b[1] if len(tuple_b) > 1 else 0
-    return (a_1 + b_1, a_2 + b_2)
+
+    # return (a_1 + b_1, a_2 + b_2)
+    print("{:d}+{:d},{:d}+{:d}".format(a_1, a_2, b_1, b_2))
