ffbfd7447e12c86dc406afa81b6cc19cef69ad27
Mar-Issah <masy370@gmail.com>
2023-05-17 12:09:04 -0400

    add new file 1-search_replace.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda newMatrix: list(map(lambda x: x**2, newMatrix)), matrix))
+    return list(map(lambda new: list(map(lambda x: x**2, new)), matrix))
