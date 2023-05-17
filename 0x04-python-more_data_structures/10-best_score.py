ec506bda9d3f94a794067a67eceff5902298e0b1
Marsiya Issah <masy370@gmail.com>
2023-05-17 14:46:48 -0400

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    best_key = max(a_dictionary, key=a_dictionary.get)
+    best_key = max(a_dictionary, key=a_dictionary.get, default=None)
     return best_key
