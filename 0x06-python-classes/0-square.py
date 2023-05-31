ce5efd6f3dee73a02e0a458328acb4aadaaeaad3
Mar-Issah <masy370@gmail.com>
2023-05-30 22:26:03 -0400

    add documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Square class module"""
+
+
 class Square:
-    """
-    This is a class Square that defines a square
-    """
+    """This is a class Square that defines a square"""
 
     pass
