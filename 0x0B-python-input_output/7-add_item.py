59ae0204143f2fd96c5b96fe62a3e28042ea5a3f
Mar-Issah <masy370@gmail.com>
2023-06-13 11:45:09 -0400

    add new file 10-student.py and implement code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,13 +9,13 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
 
-file = "add_item.json"
+json = "add_item.json"
 arr = []
 
-if os.path.exists(file):
-    arr = load_from_json_file(file)
+if os.path.exists(json):
+    arr = load_from_json_file(json)
 
 for arg in range(1, len(sys.argv)):
     arr.append(sys.argv[arg])
 
-save_to_json_file(arr, file)
+save_to_json_file(arr, json)
