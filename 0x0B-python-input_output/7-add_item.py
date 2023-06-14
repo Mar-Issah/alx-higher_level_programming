2ad3ed94082de9333294419e6549019eec90c316
Mar-Issah <masy370@gmail.com>
2023-06-14 08:35:47 -0400

    update file add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""Imported module for add_item"""
+"""Imported module for adding items to a JSON file"""
 import sys
 import json
 import os.path
 
-
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
-
-json = "add_item.json"
+json_file = "add_item.json"
 arr = []
 
-if os.path.exists(json):
-    arr = load_from_json_file(json)
+if os.path.exists(json_file):
+    arr = load_from_json_file(json_file)
 
 for arg in range(1, len(sys.argv)):
     arr.append(sys.argv[arg])
 
-save_to_json_file(arr, json)
+save_to_json_file(arr, json_file)
