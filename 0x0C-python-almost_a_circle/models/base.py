50731808e3e001a983961697b97d89e8bd72c0fb
Mar-Issah <masy370@gmail.com>
2023-06-15 15:05:40 -0400

    add new foldder models and file base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Base:
+    """The Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """The constructor method"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
