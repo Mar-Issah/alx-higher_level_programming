ffaa8d379eea62795102c9073479a9663a5ae3f5
Mar-Issah <masy370@gmail.com>
2023-06-19 18:50:36 -0400

    implement from json import loads function in base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Imported modules for the Base class"""
+from json import dumps
+from json import loads
 
 
 class Base:
@@ -14,3 +16,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Converts a list of dictionaries to JSON string"""
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Converts a JSON string to a list of dictionaries"""
+        if json_string is None or not json_string:
+            return []
+        return loads(json_string)
