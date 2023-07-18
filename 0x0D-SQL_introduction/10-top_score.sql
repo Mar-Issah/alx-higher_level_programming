719a11ee21937e27bc4a20b3db551d13ee737cdb
Mar-Issah <masy370@gmail.com>
2023-07-18 19:06:01 -0400

    add script that shows record in second table

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
--- 10-top_score.sql
--- lists all records of the table second_table
+-- script that lists all records of the table second_table
+-- cat 10-top_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 SELECT score, name FROM second_table ORDER BY score DESC;
