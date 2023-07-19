269866dbf1fc79ba9dcba106ab96f4b139eebf00
Mar-Issah <masy370@gmail.com>
2023-07-19 14:57:43 -0400

    add script that creates table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,7 @@
--- 6-states.sql
--- creates the database hbtn_0d_usa and the table states (in the database
-	-- hbtn_0d_usa) on your MySQL server.
+-- script that creates the database hbtn_0d_usa and the table states
+-- cat 6-states.sql | mysql -hlocalhost -uroot -p
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
-) ENGINE=INNODB;
+);
