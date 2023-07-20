857fcb6c28dfd3cec002fe724ba393e294cc2c8e
Mar-Issah <masy370@gmail.com>
2023-07-19 21:02:07 -0400

    add script that list tv genre if comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,8 @@
--- 15-comedy_only.sql
---  lists all Comedy shows in the database hbtn_0d_tvshows.
-SELECT s.title FROM tv_genres g, tv_show_genres t, tv_shows s
-WHERE g.id = t.genre_id
-	AND t.show_id = s.id
-	AND g.name = "Comedy"
-ORDER BY s.title ASC;
-;
+-- script that lists all Comedy shows in the database hbtn_0d_tvshows.
+-- cat 15-comedy_only.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
