13f1685aa123d0b7354e584b1a5f98da3480e371
Mar-Issah <masy370@gmail.com>
2023-08-14 13:31:28 -0400

    add new file 10-factorial.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let num = parseInt(process.argv[2]);
+const num = parseInt(process.argv[2]);
 if (isNaN(num)) console.log('Missing number of occurrences');
 for (let i = 0; i < num; i++) {
   console.log('C is fun');
