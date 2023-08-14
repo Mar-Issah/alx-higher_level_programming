126c7d467c653df2eb3c431a75170c8e35b50b95
Mar-Issah <masy370@gmail.com>
2023-08-14 13:04:21 -0400

    add new file -multi_c.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let idx = 0; idx < array.length; idx++) {
   console.log(array[idx]);
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-s;
+let num = parseInt(process.argv[2]);
+if (!isNaN(num)) console.log('Missing number of occurrences');
+for (let i = 0; i < num; i++) {
+  console.log('C is fun');
+}
