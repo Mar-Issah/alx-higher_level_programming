47eb6b0d71a3dd87a74b6649a974cd14c43cf1b1
Mar-Issah <masy370@gmail.com>
2023-08-14 13:23:46 -0400

    add new file 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,6 +3,4 @@ function add(a, b) {
   return parseInt(a) + parseInt(b);
 }
 
-const num1 = process.argv[2];
-const num2 = process.argv[3];
-console.log(add(num1, num2));
+console.log(add(process.argv[2], process.argv[3]));
