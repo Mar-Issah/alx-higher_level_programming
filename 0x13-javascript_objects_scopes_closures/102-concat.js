b2080f18284bf5a838a4b0f4061c17a4a67194ab
Mar-Issah <masy370@gmail.com>
2023-08-15 17:28:39 -0400

    update file 102-concat.js file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,19 +1,14 @@
-// #!/usr/bin/node
+#!/usr/bin/node
 const fs = require('fs');
 
-async function concatFiles() {
-  try {
-    const content1 = await fs.promises.readFile(process.argv[2], 'utf-8');
-    const content2 = await fs.promises.readFile(process.argv[3], 'utf-8');
-
-    const concatContent = content1 + '\n' + content2;
-
-    await fs.promises.writeFile(process.argv[4], concatContent);
-
-    console.log(concatContent);
-  } catch (error) {
-    console.error('An error occurred:', error.message);
-  }
-}
-
-concatFiles();
+fs.readFile(process.argv[2], (error, data) => {
+  if (error) throw error;
+  let content = data.toString();
+  fs.readFile(process.argv[3], (error, data) => {
+    if (error) throw error;
+    content += data.toString();
+    fs.writeFile(process.argv[4], content, (error) => {
+      if (error) throw error;
+    });
+  });
+});
