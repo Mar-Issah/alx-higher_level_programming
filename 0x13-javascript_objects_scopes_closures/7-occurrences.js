2ec2daf252604ed8df39efe428cd718091a9448e
Mar-Issah <masy370@gmail.com>
2023-08-15 15:12:07 -0400

    add new 9-logme.js file

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,8 +3,8 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
 
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] === searchElement) {
+  for (let idx = 0; idx < list.length; idx++) {
+    if (list[idx] === searchElement) {
       count++;
     }
   }
