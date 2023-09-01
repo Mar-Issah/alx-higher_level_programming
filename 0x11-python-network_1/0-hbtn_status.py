b235bd8b4a5b8c623bfd83f8dce8e4b75811ca95
Mar-Issah <masy370@gmail.com>
2023-09-01 15:41:05 -0400

    add new file 1-hbtn_header.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,4 +6,9 @@ import urllib.request as request
 if __name__ == "__main__":
     with request.urlopen("https://intranet.hbtn.io/status") as response:
         body = response.read()
-        print(f"Body response:\n\t- type: {type(body)}\n\t- content: {body}\n\t- utf8 content: {body.decode('utf-8')}")
+        print(
+            f"Body response:\n"
+            f"\t- type: {type(body)}\n"
+            f"\t- content: {body}\n"
+            f"\t- utf8 content: {body.decode('utf-8')}"
+        )
