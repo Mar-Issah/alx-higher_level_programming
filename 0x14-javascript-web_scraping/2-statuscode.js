2bc18c5178e67efc0a9007ac9f530fc4cae56bda
Mar-Issah <masy370@gmail.com>
2023-09-26 12:25:38 -0400

    update new file 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // script that display the status code of a GET request
 const request = require('request');
-request(process.argv[2], function (error, response) {
+request(process.argv[2], (error, response) => {
   if (error) {
     console.log(error);
   } else {
-    'code:', response.statusCode;
+    console.log('code:', response.statusCode);
   }
 });
