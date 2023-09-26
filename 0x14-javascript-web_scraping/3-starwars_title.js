24d94efda06b13b761612d0ee633cf1d8df16b06
Mar-Issah <masy370@gmail.com>
2023-09-26 13:32:35 -0400

    add ne file 6-completed_tasks.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 //script that prints the title of a Star Wars movie where the episode number matches a given integer.
 const request = require('request');
-const id = process.argv[2]
-request(`https://swapi-api.hbtn.io/api/films/${id}`, (error,res, body) => {
+const id = process.argv[2];
+request(`https://swapi-api.hbtn.io/api/films/${id}`, (error, res, body) => {
   if (error) {
     console.error(error);
   } else {
